ee84c76edd34fe83d9dd396998306ca61feff4a4
Tanushree Jain <83875193+tj4702@users.noreply.github.com>
2026-09-04 01:35:47 -0400

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        dist  = [[float('inf')] * n for _ in range(n)]
+
+        for u,v,w in times:
+            dist[u-1][v-1] = w
+        
+        for i in range(n):
+            dist[i][i] = 0 
+
+        for mid in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j] , dist[i][mid]+ dist[mid][j])
+        
+        res = max(dist[k-1])
+
+        return res if res < float('inf') else - 1
